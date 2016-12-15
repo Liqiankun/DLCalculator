a6b15d0ef8dafc88657f550c4f3712cf9d254add
Liqiankun <qiankunli@yahoo.com>
2016-12-15 10:58:10 +0800

    Upload DLCalculator

--- DLCalculator/DLCalculator/ViewController.swift
+++ DLCalculator/DLCalculator/ViewController.swift
@@ -7,19 +7,40 @@
 //
 
 import UIKit
-
+import AVFoundation
 class ViewController: UIViewController {
-
+    
+    var soundPlayer:AVAudioPlayer!
+    @IBOutlet weak var counterLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        setupSoundPlayer()
+    }
+    
+    @IBAction func numberButtonPress(sender:UIButton){
+        playSound()
+        counterLabel.text = counterLabel.text?.appending("1")
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
+    func setupSoundPlayer() {
+        let soundPath = Bundle.main.path(forResource: "btn", ofType: "wav")
+        let soundURL = URL.init(fileURLWithPath:soundPath!)
+        do {
+            try  soundPlayer = AVAudioPlayer(contentsOf: soundURL)
+            soundPlayer.prepareToPlay()
+        }catch let error as NSError{
+            print(error.debugDescription)
+        }
         
     }
-
+    
+    func playSound() {
+        if soundPlayer.isPlaying {
+            soundPlayer.stop()
+        }
+        soundPlayer.play()
+    }
 
 }
 
